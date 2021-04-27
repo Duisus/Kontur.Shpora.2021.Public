00e4208627315ee7a561c630e68b49c7fdb041de
Denis Bautin <denisbduis@gmail.com>
2021-04-28 01:33:00 +0500

    Add new methods in ClusterClientBase

--- ClusterClient/Clients/ClusterClientBase.cs
+++ ClusterClient/Clients/ClusterClientBase.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using ClusterClient.AdditionalClasses;
 using log4net;
 
 namespace ClusterClient.Clients
@@ -11,6 +14,7 @@ namespace ClusterClient.Clients
     public abstract class ClusterClientBase
     {
         protected string[] ReplicaAddresses { get; set; }
+        protected string QueryParamName { get; set; } = "query";
 
         protected ClusterClientBase(string[] replicaAddresses)
         {
@@ -39,6 +43,47 @@ namespace ClusterClient.Clients
                 Log.InfoFormat("Response from {0} received in {1} ms", request.RequestUri, timer.ElapsedMilliseconds);
                 return result;
             }
+        }
+        
+        protected async Task<IEnumerable<HttpWebRequest>> CreateRequestsAsync(
+            IEnumerable<string> addresses, string query)
+        {
+            return await Task.Run(() => addresses.Select(uri =>
+                CreateRequest(GetUriWithQuery(uri, query))));
+        }
+        
+        protected async Task<RequestResult<string>> SendRequestAsync(WebRequest request, TimeSpan timeout)
+        {
+            var timer = Stopwatch.StartNew();
+            var requestTask = ProcessRequestAsync(request);
+            var timeoutTask = CreateTimeoutTask(timeout);
+
+            await Task.WhenAny(requestTask, timeoutTask);
+            timer.Stop();
+
+            if (timeoutTask.IsCompleted)
+                return new RequestResult<string>(RequestStatus.TimeoutExceed, timeout);
+
+            return requestTask.IsFaulted
+                ? new RequestResult<string>(RequestStatus.BadResponse, timer.Elapsed)
+                : new RequestResult<string>(requestTask.Result, timer.Elapsed);
+        }
+
+        protected void ThrowTimeoutExceptionIfItExceed(RequestResult<string> requestResult)
+        {
+            if (requestResult.Status == RequestStatus.TimeoutExceed)
+                throw new TimeoutException(
+                    $"Timeout in {requestResult.Duration} ms was exceeded");
+        }
+        
+        protected static Task CreateTimeoutTask(TimeSpan timeout)
+        {
+            return Task.Delay(timeout);
+        }
+
+        protected string GetUriWithQuery(string uri, string paramValue)
+        {
+            return $"{uri}?{QueryParamName}={paramValue}";
         }
     }
 }
